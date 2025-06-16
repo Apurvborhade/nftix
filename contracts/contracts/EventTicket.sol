710b49bbbec8a991e8423d54da595aaf4c8dd2b8
Apurvborhade <apurvaborhadee@gmail.com>
2025-06-16 16:41:52 +0530

    Ticket Contract init

---
+++ contracts/contracts/EventTicket.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.27;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract EventTicket is ERC721, Ownable {
+    uint public maxSupply = 100;
+    uint public currentSupply;
+    uint public mintPrice = 0.01 ether;
+    address public organizer;
+
+    constructor(
+        string memory _tokenName,
+        string memory _tokenSymbol,
+        address initialOwner
+    ) ERC721(_tokenName, _tokenSymbol) Ownable(initialOwner) {
+        organizer = msg.sender;
+    }
+
+    event TicketMinted(address, uint);
+    // Mint Ticket
+    function mintTicket(address to, uint256 tokenId) public payable onlyOwner {
+        // Check for Supply
+        require(currentSupply < maxSupply, "Maximum Supply Reached");
+
+        // Check The Amount of ether sent
+        require(msg.value == mintPrice, "Price of this ticket is 0.01 ETH");
+        
+        _safeMint(to, tokenId);
+        
+        // Emit Event
+        emit TicketMinted(to, tokenId);
+        currentSupply++;
+    }
+
+    // Read Organizer
+    function viewOrganizer() public view returns (address) {
+        return organizer;
+    }
+
+    // Ticket Price
+    function ticketPrice() public view returns (uint) {
+        return mintPrice;
+    }
+}
